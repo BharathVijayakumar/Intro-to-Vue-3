62bb527d6b45e0a5bae9604b1c36aef048fb12ef
Milan <milanreq@gmail.com>
2021-07-24 19:38:22 -0400

    L8-Course Computed Properties

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ const app = Vue.createApp({
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantitiy:50, onSale: true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantitiy:0 },
             ]
         }
     },
@@ -17,8 +17,20 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed:{
+        title(){
+            return `${this.brand} ${this.product}`;
+        },
+        howeredVariant(){
+            const variant = this.variants[this.selectedVariant];
+            return { ...variant, inStock : variant.quantitiy > 0}
+        },
+        onSale(){
+            return this.variants[this.selectedVariant].onSale? '10 times on Sale': '';
         }
     }
 })
