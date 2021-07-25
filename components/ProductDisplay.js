abcedfff2822731a30d446e8671c1c8162945ded
Milan <milanreq@gmail.com>
2021-07-25 16:27:32 -0400

    L10-Course Events

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,14 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="removeItemFromcart">
+          Remove from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,10 +64,13 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      removeItemFromcart(){
+        this.$emit('remove-from-cart')
       }
   },
   computed: {
